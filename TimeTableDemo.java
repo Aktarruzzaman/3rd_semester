462031d0b006612c1571c09ad450add80d548926
Aktarruzzaman Rifat <blogrifat6@gmail.com>
2024-09-18 02:06:41 +0600

    Extract username password with java programming

---
+++ TimeTableDemo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TimeTableDemo {
+        public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+
+
+        int n,m;
+        System.out.print("Enter Initital number=");
+        n = input.nextInt();
+
+        System.out.print("Enter Final number:");
+        m = input.nextInt();
+
+        for(int i=m; i<=n; i++){
+     
+           for(int j=1; j<=10; j++){
+          
+             System.out.println(i+" x "+j +" = "+i*j);   
+          }
+             
+          System.out.print("\n\n");
+      }
+   
+    }
+    
+}
