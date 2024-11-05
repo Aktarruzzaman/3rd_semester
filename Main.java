f064fd61a72a5a895fc188f9ad73bcd60b35eb20
Aktarruzzaman Rifat <blogrifat6@gmail.com>
2024-11-06 00:39:25 +0600

    java programming with Ternary Operator
     java programming with Ternary Operator

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.sql.Time;
+
+public class Main {
+    public static void main(String[]args)
+    {
+        Ternary ob = new Ternary();
+       System.out.println(ob.iseven(5));
+       System.out.println(ob.ispositive(7));
+       System.out.println(ob.greater(7,21));
+       System.out.println(ob.smaller(34,12));
+       System.out.println(ob.isEqual(34,12));
+
+    }
+}
---
+++ Ternary.java
@@ -0,0 +1,23 @@
+public class Ternary {
+    public boolean iseven(int n)
+    {
+        return n/2== 0 ? true:false;
+    }
+    public boolean ispositive(int n)
+    {
+         return n>0 ? true:false;
+    }
+    public int greater(int a,int b)
+    {
+        return a>b ? a:b;
+    }
+    public int smaller(int a, int b)
+    {
+        return a<b ? a:b;
+    }
+    public boolean isEqual(int a,int b)
+    {
+        return a==b ? true:false;
+    }
+
+}
